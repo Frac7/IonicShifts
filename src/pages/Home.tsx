7b5b44b9fb476e5dd3317f131a666be9e5cd37bf
Frac7 <francescacella96@gmail.com>
2020-12-19 23:26:52 +0100

    Home updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {
     IonList,
@@ -7,7 +7,8 @@ import {
     IonText,
     IonIcon,
     IonRow,
-    IonCol
+    IonCol,
+    IonSpinner
 } from '@ionic/react';
 
 import { arrowDropright } from 'ionicons/icons'
@@ -18,35 +19,55 @@ type HomeProps = {
     history: any
 }
 
-const Home: React.FC<HomeProps> = ({ history }: HomeProps) => (
-    <BasePage title="Turni Acquisto Prodotti" content={
-        <IonList>
-            {[
-                { item: 'Scottex', current: 'Francesca C.'},
-                { item: 'Detersivo Lavastoviglie', current: 'Fra e Chiara'},
-                { item: 'Detersivo Piatti', current: 'Alessandro'},
-                { item: 'Buste', current: 'Giuseppe'},
-                ].map(({ item, current}, index) => (
-                <IonItem onClick={() => history.push(`/details/${index}`)} key={index}>
-                    <IonLabel>
-                        <IonRow className="ion-align-items-center ion-justify-content-between">
-                            <IonCol size="10">
-                                <IonText color="primary">
-                                    <h1>{item}</h1>
-                                </IonText>
-                                <IonText>
-                                    <p>{current}</p>
-                                </IonText>
-                            </IonCol>
-                            <IonCol size="auto">
-                                <IonIcon size="large" icon={arrowDropright}/>
-                            </IonCol>
-                        </IonRow>
-                    </IonLabel>
-                </IonItem>
-            ))}
-        </IonList>
-    } />
-);
+const Home: React.FC<HomeProps> = ({ history }: HomeProps) => {
+    const [ items, setItems ] = useState([]);
+
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ isError, setIsError ] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/products')
+            .then((res) => res.json())
+            .then((res) => {
+                setItems(res);
+                setIsLoading(false);
+            })
+            .catch(() => {
+                setIsError(true);
+                setIsLoading(false);
+            });
+    }, []);
+
+    if (isLoading) {
+        return <IonSpinner color="primary" />
+    }
+
+    if (isError) {
+        return <IonText color="danger">Si è verificato un errore.</IonText>
+    }
+
+    return (
+        <BasePage title="Turni Acquisto Prodotti" content={
+            <IonList>
+                {items.map(({ name }, index) => (
+                    <IonItem onClick={() => history.push(`/details/${index}`)} key={index}>
+                        <IonLabel>
+                            <IonRow className="ion-align-items-center ion-justify-content-between">
+                                <IonCol size="10">
+                                    <IonText color="primary">
+                                        <h1>{name}</h1>
+                                    </IonText>
+                                </IonCol>
+                                <IonCol size="auto">
+                                    <IonIcon size="large" icon={arrowDropright}/>
+                                </IonCol>
+                            </IonRow>
+                        </IonLabel>
+                    </IonItem>
+                ))}
+            </IonList>
+        } />
+    );
+}
 
 export default Home;
